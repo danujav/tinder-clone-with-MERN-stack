39e0e3f4c1c573dbd90d2d7cc220881db4b6423e
DanujaV <danujagreru@gmail.com>
2021-08-21 16:10:25 +0530

    done header

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,10 +1,15 @@
 import React from "react"
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Let's build this</h1>
+    <div className="app">
+      
+      <Header/>
+      {/* Header */}
+      {/* TinderCards */}
+      {/* SwipeButton */}
     </div>
   );
 }
---
+++ tinder-clone/src/TinderCards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./TinderCards.css";
+
+function TinderCards() {
+    return (
+        <div className="tinderCards">
+            
+        </div>
+    )
+}
+
+export default TinderCards;
