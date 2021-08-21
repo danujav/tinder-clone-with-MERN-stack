0815fa12a5c1754bdea387cd797b2f7ae098aa23
DanujaV <danujagreru@gmail.com>
2021-08-21 19:06:17 +0530

    complete front-end with react

--- tinder-clone/src/Header.js
+++ tinder-clone/src/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./Header.css";
 import PersonIcon from '@material-ui/icons/Person';
-import IconButton from "@material-ui/core/IconButton";
+import IconButton from '@material-ui/core/IconButton';
 import ForumIcon from '@material-ui/icons/Forum';
 
 function Header() {
