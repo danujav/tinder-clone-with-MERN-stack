23b364b46dd10a1d2df8981e47af9b59e9f5454e
DanujaV <danujagreru@gmail.com>
2021-08-21 17:12:41 +0530

    complete tinder cards

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -1,14 +1,14 @@
 import React from "react"
 import './App.css';
 import Header from "./Header";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
     <div className="app">
       
       <Header/>
-      {/* Header */}
-      {/* TinderCards */}
+      <TinderCards/>
       {/* SwipeButton */}
     </div>
   );
--- tinder-clone/src/TinderCards.js
+++ tinder-clone/src/TinderCards.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
 
 function TinderCards() {
+    const [people, setPeople] = useState([
+        {
+            name: 'Elon Musk',
+            url: "https://www.aljazeera.com/wp-content/uploads/2021/07/AP21190798629710.jpg?resize=770%2C513"
+        },
+        {
+            name: 'Jeff Bezos',
+            url: "https://wallpapercave.com/wp/wp4025589.jpg"
+        },
+        {
+            name: 'Ellen',
+            url: "https://wallpapercave.com/wp/wp2249449.jpg"
+        },
+        {
+            name: 'Bill Gates',
+            url: "https://wallpapercave.com/wp/OF3Zstl.jpg"
+        },
+        {
+            name: 'Steve Job',
+            url: "https://wallpapercave.com/wp/mJRp4Xn.jpg"
+        },
+    ]);
+
+    const swiped = (direction, nameToDelete) => {
+        console.log("removing: " + nameToDelete);
+    };
+
+    const outOfFrame = (name) => {
+        console.log(name + " Left the screen..!")
+    };
+
     return (
         <div className="tinderCards">
-            
+            <div className="tinderCards__cardContainer">
+                {people.map(person => (
+                    <TinderCard
+                        className="swipe"
+                        key={person.name}
+                        preventSwipe={["up", "down"]}
+                        onSwipe={(dir) => swiped(dir, person.name)}
+                        onCardLeftScreen={() => outOfFrame(person.name)}
+                    >
+                        <div 
+                            style={{backgroundImage: `url(${person.url})`}}
+                            className="card"
+                        >
+                            <h3>{person.name}</h3>
+                        </div>
+
+                    </TinderCard>
+                ))}
+            </div>
         </div>
     )
 }
